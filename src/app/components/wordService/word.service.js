4c59b0215229dac5a9a268802a75cd3867fc55cf
bylertall <tball88@gmail.com>
2015-08-31 20:48:03 -0700

    get word & scramble in controller, show in view, some style added

--- src/app/components/wordService/word.service.js
+++ src/app/components/wordService/word.service.js
@@ -11,7 +11,7 @@
         apiWordEnd = 'randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=6&maxLength=6&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
         factory = {
           word: ''
-    };
+        };
 
     factory.getNewWord = function() {
       return $http.get(apiBase + apiWordEnd)
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,8 +6,48 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($document, wordService) {
+  function MainController(wordService) {
     var vm = this;
 
+    function getWord() {
+      wordService.getNewWord()
+        .success(
+        function(data) {
+          vm.word = data.word;
+          vm.scrambledWord = scramble(vm.word);
+        }).error(
+        function(data) {
+          console.log('An error occurred when getting the word');
+          console.log(data);
+        }
+      );
+    }
+
+    function scramble(word) {
+      var array = word.toUpperCase().split(''),
+        counter = array.length,
+        temp,
+        i;
+
+      // scramble letters (REMEMBER CONCEPT: Fisher-Yates shuffle)
+      while (counter) {
+
+        // integer to select random array index
+        i = Math.floor(Math.random() * counter--);
+
+        // item at end of initial array is set to temp
+        temp = array[counter];
+
+        // random array item is set at current counter position of array
+        array[counter] = array[i];
+
+        //  move temp to the random array index
+        array[i] = temp;
+      }
+
+      return array;
+    }
+
+    getWord();
   }
 })();
