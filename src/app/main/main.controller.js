2f5c0e6677bcaca909c5dd04343606c881a28077
bylertall <tball88@gmail.com>
2015-09-02 07:39:39 -0700

    update style

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,17 +9,16 @@
   function MainController($document, $timeout, wordService) {
     var vm = this;
 
-    vm.wordService = wordService;
-    vm.word = vm.wordService.word;
-    vm.scrambledArray = vm.wordService.scrambledArray;
-    vm.userInput = vm.wordService.userInput;
+    vm.word = wordService.word;
+    vm.scrambledArray = wordService.scrambledArray;
+    vm.userInput = wordService.userInput;
 
     // bind keydown event and check if character has a match in scramble array
     $document.bind('keydown', function(keyEvent) {
       var timer;
 
       $timeout(function() {
-        vm.wordService.checkUserInput(keyEvent);
+        wordService.checkUserInput(keyEvent);
       });
     });
   }
